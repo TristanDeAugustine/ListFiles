3ebebe949ea78c374cddc835300a73903427dfd6
Tristan DeAugustine <deaugustinetristan@gmail.com>
2021-01-03 13:52:53 -0500

    used json.net to pull data fields from json file

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json.Linq;
+
+static void ListFiles()
+{
+    var json = File.ReadAllText(@"Omnis.json");
+    try
+    {
+        var jObject = JObject.Parse(json);
+
+        if (jObject != null)
+        {
+            Console.WriteLine("HOSTNAME :" + jObject["hostName"].ToString());
+            Console.WriteLine("PACKAGEVERSION :" + jObject["packageVersion"].ToString());
+            Console.WriteLine("DATE TIME: " + jObject["dateTime"].ToString());
+            Console.WriteLine("TYPE: " + jObject["type"].ToString());
+
+            JArray versionsArrary = (JArray)jObject["Versions"];
+            if (versionsArrary != null)
+            {
+                foreach (var item in versionsArrary)
+                {
+                    Console.WriteLine("Version :" + item["version"]);
+                }
+
+            }
+            JArray usersArrary = (JArray)jObject["Users"];
+            if (usersArrary != null)
+            {
+                foreach (var item in usersArrary)
+                {
+                    Console.WriteLine("User :" + item["user"]);
+                }
+
+            }
+        }
+    }
+    catch (Exception)
+    {
+
+        throw;
+    }
+
+}
